36e9df107b4c03b15f663c18f66e2fd3e53a00ca
Marcheno1959 <marcheno1959@gmail.com>
2023-06-21 15:41:40 +0200

    Updated city variable for testing the app

--- src/app.js
+++ src/app.js
@@ -79,11 +79,13 @@ function displayTemperature(response) {
 /* step 9 - to display the real temperature in the 'weather temperature' element, remove the hard-coded value in HTML and add an id="temperature" */
 /* step 10 - to display the real city in the 'h1' element, remove the hard-coded city in HTML and add an id="city" */
 /* step 11 - to display the real weather condition in the 'li' element, remove the hard-coded description in HTML and add an id="description" */
+let city = "Koksijde";
+
 let apiKey =
   "94f405d2060ftof44a10fbe606f73f39"; /* step 3 - store API key in a variable for authentication*/
 
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=Barcelona&key=${apiKey}&units=metric`; /* step 4 - get documentation for current weather URL endpoint & paste here */
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`; /* step 4 - get documentation for current weather URL endpoint & paste here */
 /* step 5 - console.log(apiUrl); */
-
+console.log(apiUrl);
 axios.get(apiUrl).then(displayTemperature);
 /* step 6 - use Axios to make the API call to fetch the results of the API to display temperatue */
