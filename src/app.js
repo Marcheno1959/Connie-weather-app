6efa36df6c1d69661a5105bea76c8eefc20815a0
Marcheno1959 <marcheno1959@gmail.com>
2023-06-21 16:14:37 +0200

    Tested city variable and amended spacing for temperature units

--- src/app.js
+++ src/app.js
@@ -79,7 +79,7 @@ function displayTemperature(response) {
 /* step 9 - to display the real temperature in the 'weather temperature' element, remove the hard-coded value in HTML and add an id="temperature" */
 /* step 10 - to display the real city in the 'h1' element, remove the hard-coded city in HTML and add an id="city" */
 /* step 11 - to display the real weather condition in the 'li' element, remove the hard-coded description in HTML and add an id="description" */
-let city = "Koksijde";
+let city = "Cancun";
 
 let apiKey =
   "94f405d2060ftof44a10fbe606f73f39"; /* step 3 - store API key in a variable for authentication*/
