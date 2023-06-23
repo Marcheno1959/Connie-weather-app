ec7136b4fe659394996cc30e7b87a36e40749c65
Marcheno1959 <marcheno1959@gmail.com>
2023-06-23 12:38:17 +0200

    Modified temperature unit conversion

--- src/app.js
+++ src/app.js
@@ -58,12 +58,20 @@ function handleSubmit(event) {
 function displayFahrenheitValue(event) {
   event.preventDefault();
   let temperatureElement = document.querySelector("#temperature");
+
+  celsiusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
+
   let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
   temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
 }
 
 function displayCelsiusValue(event) {
   event.preventDefault();
+
+  celsiusLink.classList.add("active");
+  fahrenheitLink.classList.remove("active");
+
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(celsiusTemperature);
 }
