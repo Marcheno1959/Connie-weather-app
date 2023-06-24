504be4c9d52b352ab18f0305a4593dbf3b890ef8
Marcheno1959 <marcheno1959@gmail.com>
2023-06-24 14:32:54 +0200

    Amended spelling error in apiUrl

--- src/app.js
+++ src/app.js
@@ -43,7 +43,7 @@ function displayForecast() {
 function getForecast(coordinates) {
   console.log(coordinates);
   let apiKey = "94f405d2060ftof44a10fbe606f73f39";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?lon=${coordinates}&lat=${coordinates}&key=${apiKeyey}&units=metric`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?lon=${coordinates}&lat=${coordinates}&key=${apiKey}&units=metric`;
   console.log(apiUrl);
 }
 
